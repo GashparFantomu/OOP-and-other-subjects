6a708f2625457375da6ba943614fe5652f6183a7
Gashpar Fantomu <TERRENEITOR>
2025-06-08 22:16:07 +0300

    cap 4 - not done yet

---
+++ INCEME SA LUCRAM LA BREAZU/operator_atribuire_exemplu_simplu.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <iostream>
+
+using namespace std;
+
+struct Student {
+    string nume;
+    int varsta;
+};
+
+int main() {
+    cout << "1) Constructor implicit + copiere:\n";
+    Student a = { "Ana", 20 };   // inițializare
+    Student b = a;             // constructor de copiere (implicit)
+
+    cout << "a.nume = " << a.nume << ", b.nume = " << b.nume << "\n";
+
+    cout << "\n2) Operator de atribuire (implicit):\n";
+    Student c = { "Bogdan", 22 };
+    c = a;                     // operator de atribuire (implicit)
+
+    cout << "c.nume = " << c.nume << ", c.varsta = " << c.varsta << "\n";
+
+    return 0;
+}
