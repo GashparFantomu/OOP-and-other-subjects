ec31e028cbcbc53ec6a93f0cc0f20fda2567c893
Gashpar Fantomu <TERRENEITOR>
2025-06-09 19:09:42 +0300

    factory method

---
+++ INCEME SA LUCRAM LA BREAZU/factory_method.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Shape {
+public:
+    virtual void draw() const = 0;
+    virtual ~Shape() {}
+};
+
+class Circle : public Shape {
+public:
+    void draw() const override {
+        cout << "Desenez un cerc\n";
+    }
+};
+
+class Square : public Shape {
+public:
+    void draw() const override {
+        cout << "Desenez un patrat\n";
+    }
+};
+
+class ShapeFactory {
+public:
+    static Shape* create(const string& type) {
+        if (type == "circle") {
+            return new Circle();
+        }
+        else if (type == "square") {
+            return new Square();
+        }
+        else {
+            return nullptr;  // tip necunoscut
+        }
+    }
+};
+
+int main() {
+    Shape* s1 = ShapeFactory::create("circle");
+    Shape* s2 = ShapeFactory::create("square");
+
+    if (s1) s1->draw();   // “Desenez un cerc”
+    if (s2) s2->draw();   // “Desenez un pătrat”
+
+    delete s1;
+    delete s2;
+}
